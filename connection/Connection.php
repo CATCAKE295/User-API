d9bce0dd1f25be8ae26b688732eb4244fc9fa9af
Francisco <66093384+CATCAKE295@users.noreply.github.com>
2023-05-10 18:12:43 -0600

    Add files via upload

---
+++ connection/Connection.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Connection extends Mysqli {
+
+    function __construct(){
+
+        parent::__construct('localhost','root','1234','codesociety_uca_user');
+
+        $this->set_charset('utf8mb4');
+        $this->connect_error == NULL ? 'Conexión exitosa a la BD!!' : die('Error al conectarse');
+    }
+}
+
+?>
